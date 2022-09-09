a130f05da546ea3384f725c5f99f183e389d60c4
isai Abraham <isaisabrahamf@gmail.com>
2022-09-09 10:58:00 -0500

    Initial Commit

---
+++ ExamenMovie/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ExamenMovie
+//
+//  Created by Isai Abraham on 09/09/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
